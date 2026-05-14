6bad5763a8b80e494ad3374f661977bc7a1edf93
Maxi Barmetler <maxi@barmetler.com>
2026-05-14 17:03:59 +0200

    try signing

--- src/main/kotlin/com/barmetler/springdemo/feature/organization/application/model/OrganizationRecord.kt
+++ src/main/kotlin/com/barmetler/springdemo/feature/organization/application/model/OrganizationRecord.kt
@@ -2,4 +2,8 @@ package com.barmetler.springdemo.feature.organization.application.model
 
 import java.util.UUID
 
-data class OrganizationRecord(val id: UUID, val slug: String, val name: String)
+data class OrganizationRecord(
+    val id: UUID,
+    val slug: String,
+    val name: String,
+)
